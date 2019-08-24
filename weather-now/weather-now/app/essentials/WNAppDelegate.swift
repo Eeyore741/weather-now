974c532a14416b4c651f92cf09620ece10f01d9e
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2019-08-24 23:47:55 +0200

    refactor app essentials structure
    Signed-off-by: Vitalii Kuznetsov <v-blacksmith@hotmail.com>

---
+++ weather-now/weather-now/app/essentials/WNAppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  WNAppDelegate.swift
+//  weather-now
+//
+//  Created by Vitalii Kuznetsov on 22/08/2019.
+//  Copyright © 2019 Vitalii Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class WNAppDelegate: UIResponder{
+
+    var window: UIWindow? = {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.backgroundColor = UIColor.black
+        return window
+    }()
+}
+
+extension WNAppDelegate: UIApplicationDelegate{
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let model = WNPredictionModel(locationProvider: HardwareLocationService(), predictionProvider: DarkSkyPredictionService(url: "https://api.darksky.net", token: "2bb07c3bece89caf533ac9a5d23d8417"))
+        let viewController = WNViewController(model)
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
