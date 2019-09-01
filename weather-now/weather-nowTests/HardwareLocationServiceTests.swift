1793e68c5b81db42633ab81d1d9e134c3b02bab9
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2019-09-01 21:41:11 +0200

    add location domain test case
    Signed-off-by: Vitalii Kuznetsov <v-blacksmith@hotmail.com>

---
+++ weather-now/weather-nowTests/HardwareLocationServiceTests.swift
@@ -0,0 +1,22 @@
+//
+//  HardwareLocationServiceTest.swift
+//  weather-nowTests
+//
+//  Created by Vitalii Kuznetsov on 01/09/2019.
+//  Copyright © 2019 Vitalii Kuznetsov. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import weather_now
+
+class HardwareLocationService: XCTestCase {
+    
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+}
